e82a5492349c57ffe2e1c3aa6dea6439122235dc
Divyansh2045 <divyanshnagar@hotmail.com>
2024-07-04 04:47:58 +0530

    Update main.cpp

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -49,59 +49,7 @@ int main (){
 
 int main()
 {
-    /* //render a blue colored window
-    sf::VideoMode videomode = sf::VideoMode(800, 600);
-    sf::RenderWindow window(videomode, "SFML Window");
-    Player player;
-
-    player.playershiptexture.loadFromFile("assets/textures/player_ship.png");
    
-    player.playershipsprite.setTexture(player.playershiptexture);                                // sprite.setTexture(texture);
-    // set position, rotation, scale and more
-
-   // player.playershipsprite.setPosition(300, 450);
-    player.playershipsprite.setRotation(0);
-    player.playershipsprite.setScale(1, 1); 
-    cout << "Player score is " << player.getScore() << endl;;
-    player.setScore(100);
-    cout <<"New player score: "<<player.getScore() << endl;
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            //Check for window closure
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
-
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-            {
-
-                player.move(-1.0f * player.getMoveSpeed());
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-            {
-
-                player.move(1.0f * player.getMoveSpeed());
-            }
-
-
-            //Clear the window
-            window.clear(sf::Color::Blue);
-
-            player.playershipsprite.setPosition(player.getPosition());
-            //Draw assets
-            window.draw(player.playershipsprite);
-            //Display window to display whatever drawn
-            window.display();
-        }
-
-
-    } */
-
-=======
         gameservice->update();
         gameservice->render();
     }
