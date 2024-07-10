154804458f17aeaa608285777bcb6fec7b12a541
Divyansh2045 <divyanshnagar@hotmail.com>
2024-07-10 13:57:38 +0530

    added code for player.h file

---
+++ Space-Invaders/HeaderFiles/PlayerService.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+
+class PlayerService
+{
+
+private:
+	int health = 3;
+	int movementSpeed = 50;
+	int playerScore = 0;
+	sf::Vector2f position = sf::Vector2f(200.0f, 100.0f);
+	
+	const sf::String player_texture_path = "assets/textures/player_ship.png";
+	
+	sf::Texture player_texture;
+	sf::Sprite player_sprite;
+	sf::RenderWindow* gameWindow();
+
+	void initializePlayerSprite();
+	void initializePlayerInput();
+
+
+
+public:
+
+	PlayerService();
+	~PlayerService();
+
+	void update();
+	void render();
+	void initialize();
+
+	void move(float offsetX);
+	int getMoveSpeed();
+	sf::Vector2f getPosition();
+
+
+
+
+
+};
