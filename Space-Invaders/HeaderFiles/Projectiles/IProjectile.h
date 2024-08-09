58dbfab4d70530ac851fbfe4a8af9b50dc9e1229
Divyansh2045 <divyanshnagar@hotmail.com>
2024-08-09 12:19:51 +0530

    added iprojectile files and bullet files

---
+++ Space-Invaders/HeaderFiles/Projectiles/IProjectile.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <SFML/System/Vector2.hpp>
+#include "../../HeaderFiles/Enemy/Controllers/EnemyConfig.h"
+//#include "../../HeaderFiles/Bullet/BulletConfig.h"
+
+namespace Projectile
+{
+
+	enum class MovememtDirection;
+	class IProjectile
+	{
+	public:
+
+		virtual void initialize(sf::Vector2f position, /*Bullet::*/MovememtDirection direction) = 0;
+
+		virtual void update() = 0;
+		virtual void render() = 0;
+
+		virtual sf::Vector2f getProjectilePosition() = 0;
+		virtual void updateProjectilePosition() = 0;
+		virtual ~IProjectile();
+	};
+
+}
---
+++ Space-Invaders/Source/Bullet/BulletService.cpp
@@ -0,0 +1,20 @@
+#include "../../HeaderFiles/Bullet/BulletService.h"
+
+
+void BulletService::initialize()
+{
+
+}
+
+void BulletService::update()
+{
+
+}
+void BulletService::render()
+{
+
+}
+void BulletService::destroy()
+{
+
+}
